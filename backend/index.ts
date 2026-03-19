a5f49be747d4fd0c38ccd03e7bc1c1692ed2d91d
giahuyduongdev <giahuyduongdev@gmail.com>
2026-03-19 10:06:53 +0700

    chore(backend): setup server with middlewares and graceful shutdown

---
+++ backend/index.ts
@@ -0,0 +1,36 @@
+import app from './src/app'
+import { Env } from './src/config/env.config'
+import connectDB from './src/config/database.config'
+import { initializeCrons } from './src/cron'
+
+const startServer = async () => {
+  try {
+    await connectDB()
+
+    if (Env.NODE_ENV === 'development') {
+      await initializeCrons()
+    }
+
+    const server = app.listen(Env.PORT, () => {
+      console.log(
+        `Server is running on port ${Env.PORT} in ${Env.NODE_ENV} mode`
+      )
+    })
+
+    const shutdown = (signal: string) => {
+      console.log(`${signal} received, shutting down...`)
+      server.close(() => {
+        console.log('Server closed')
+        process.exit(0)
+      })
+    }
+
+    process.on('SIGTERM', () => shutdown('SIGTERM'))
+    process.on('SIGINT', () => shutdown('SIGINT'))
+  } catch (error) {
+    console.error('Failed to start server:', error)
+    process.exit(1)
+  }
+}
+
+startServer()
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,50 +1,41 @@
 import 'dotenv/config'
 import './config/passport.config'
+import './config/redis.config'
 import passport from 'passport'
 import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 import { Env } from './config/env.config'
 import { HTTPSTATUS } from './config/http.config'
 import { errorHandler } from './middlewares/errorHandler.middleware'
 import { BadRequestException } from './utils/app-error'
 import { asyncHandler } from './middlewares/asyncHandler.middleware'
-import connectDB from './config/database.config'
+import { passportAuthenticateJwt } from './config/passport.config'
+import { checkBlacklist } from './middlewares/blacklist.middleware'
 import authRoutes from './routes/auth.route'
 import userRoutes from './routes/user.route'
 import transactionRoutes from './routes/transaction.route'
-import { passportAuthenticateJwt } from './config/passport.config'
-import { initializeCrons } from './cron'
 import reportRoutes from './routes/report.route'
 import analyticsRoutes from './routes/analytics.route'
-import './config/redis.config'
-import { checkBlacklist } from './middlewares/blacklist.middleware'
-import cookieParser from 'cookie-parser'
-
+import morgan from 'morgan'
+import helmet from 'helmet'
+import compression from 'compression'
 const app = express()
 const BASE_PATH = Env.BASE_PATH
 
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(compression())
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
 app.use(cookieParser())
-
 app.use(passport.initialize())
 app.use(checkBlacklist)
-
 app.use(
   cors({
     origin: Env.FRONTEND_ORIGIN,
     credentials: true
-  })
-)
-
-app.get(
-  '/',
-  asyncHandler((req: Request, res: Response, next: NextFunction) => {
-    throw new BadRequestException('This is a test error')
-    res.status(HTTPSTATUS.OK).json({
-      message: 'Hello'
-    })
   })
 )
 
@@ -56,11 +47,4 @@ app.use(`${BASE_PATH}/analytics`, passportAuthenticateJwt, analyticsRoutes)
 
 app.use(errorHandler)
 
-app.listen(Env.PORT, async () => {
-  await connectDB()
-
-  if (Env.NODE_ENV === 'development') {
-    await initializeCrons()
-  }
-  console.log(`Server is running on port ${Env.PORT} in ${Env.NODE_ENV} mode`)
-})
+export default app
